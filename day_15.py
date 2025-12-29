71b048da115b43190aa971c3ca2ab62b7a5fc438
Kumar <91068486+inv-fourier-transform@users.noreply.github.com>
2025-12-29 21:00:34 +0530

    Day 15

---
+++ day_15.py
@@ -0,0 +1,27 @@
+import time
+
+def timerdecorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"The function took {(end_time - start_time) * 1000} milliseconds to execute")
+        return result
+
+    return wrapper
+
+def fib(n:int) -> int:
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer")
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+@timerdecorator
+def timed_fib(n:int) -> int:
+    return fib(n)
+
+if __name__ == "__main__":
+    n = int(input("Enter a number: "))
+    print(timed_fib(n))
